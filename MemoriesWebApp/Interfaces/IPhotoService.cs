33db08ba4a5808fac06bb9ffe99a01864046834c
calKU0 <228528@student.uek.krakow.pl>
2024-04-25 01:54:42 +0200

    integrate cloudinary photo upload

---
+++ MemoriesWebApp/Interfaces/IPhotoService.cs
@@ -0,0 +1,10 @@
+﻿using CloudinaryDotNet.Actions;
+
+namespace MemoriesWebApp.Interfaces
+{
+    public interface IPhotoService
+    {
+        Task<ImageUploadResult> AddPhotoAsync(IFormFile file);
+        Task<DeletionResult> DeletePhotoAsync(string publicId);
+    }
+}
---
+++ MemoriesWebApp/Services/PhotoService.cs
@@ -0,0 +1,45 @@
+﻿using CloudinaryDotNet;
+using CloudinaryDotNet.Actions;
+using MemoriesWebApp.Helpers;
+using MemoriesWebApp.Interfaces;
+using Microsoft.Extensions.Options;
+
+namespace MemoriesWebApp.Services
+{
+    public class PhotoService : IPhotoService
+    {
+        private readonly Cloudinary _cloudinary;
+        public PhotoService(IOptions<CloudinarySettings> config)
+        {
+            var acc = new Account(
+                config.Value.CloudName,
+                config.Value.ApiKey,
+                config.Value.ApiSecret
+                );
+            _cloudinary = new Cloudinary(acc);
+        }
+        public async Task<ImageUploadResult> AddPhotoAsync(IFormFile file)
+        {
+            var uploadResult = new ImageUploadResult();
+            if(file != null)
+            {
+                using var stream = file.OpenReadStream();
+                var uploadParams = new ImageUploadParams
+                {
+                    File = new FileDescription(file.Name, stream),
+                    Transformation = new Transformation().Height(450).Width(800).Crop("fill").Gravity("face")
+                };
+                uploadResult = await _cloudinary.UploadAsync(uploadParams);
+            }
+            return uploadResult;
+        }
+
+        public async Task<DeletionResult> DeletePhotoAsync(string publicId)
+        {
+            var deleteParams = new DeletionParams(publicId);
+            var result = await _cloudinary.DestroyAsync(deleteParams);
+
+            return result;
+        }
+    }
+}
