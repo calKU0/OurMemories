5f0a80dd89db21f45483cd7be788b15a9a2040e2
Krzysztof Kurowski <228528@student.uek.krakow.pl>
2024-04-24 15:15:41 +0200

    add Image View and Controller

--- MemoriesWebApp/Models/Image.cs
+++ MemoriesWebApp/Models/Image.cs
@@ -9,6 +9,8 @@ namespace MemoriesWebApp.Models
         public int Id { get; set; }
         public string? Name { get; set; }
         public string? Description { get; set; }
+        public string? City { get; set; }
+        public DateOnly Date  { get; set; }
         public string Url { get; set; }
         [ForeignKey("Meeting")]
         public int MeetingId { get; set; }
