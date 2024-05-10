338a74ffd88d2ce4c3234a60f157f162d79dde77
Krzysztof Kurowski <228528@student.uek.krakow.pl>
2024-05-10 14:35:31 +0200

    create Edit subpage for Image controller

---
+++ MemoriesWebApp/ViewModels/EditImageViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace MemoriesWebApp.ViewModels
+{
+    public class EditImageViewModel
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public string? City { get; set; }
+        public DateOnly Date { get; set; }
+        public IFormFile? Image { get; set; }
+        public string Url { get; set; }
+        public int MeetingId { get; set; }
+    }
+}
