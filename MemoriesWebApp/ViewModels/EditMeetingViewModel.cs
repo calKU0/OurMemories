177b7781398f6c5838dfe42fea3c5a7fa139c610
Krzysztof Kurowski <228528@student.uek.krakow.pl>
2024-05-10 14:33:34 +0200

    change photo upload so the photo doesnt replace if not changed

---
+++ MemoriesWebApp/ViewModels/EditMeetingViewModel.cs
@@ -0,0 +1,15 @@
+﻿using MemoriesWebApp.Data.Enum;
+
+namespace MemoriesWebApp.ViewModels
+{
+    public class EditMeetingViewModel
+    {
+        public int Id { get; set; }
+        public DateTime DateStart { get; set; }
+        public DateTime DateEnd { get; set; }
+        public MeetingCity MeetingCity { get; set; }
+        public IFormFile? Image { get; set; }
+        public string? Url { get; set; }
+        public bool Realized { get; set; }
+    }
+}
