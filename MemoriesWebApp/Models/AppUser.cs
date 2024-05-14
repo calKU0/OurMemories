b7ae4d75a48c75bd1c933c1ec1b2dd5f0dd9e000
calKU0 <228528@student.uek.krakow.pl>
2024-05-14 22:04:34 +0200

    add base register and login functionalites

---
+++ MemoriesWebApp/Models/AppUser.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace MemoriesWebApp.Models
+{
+    public class AppUser : IdentityUser
+    {
+        public ICollection<Image> Images { get; set; }
+        public ICollection<Meeting> Meetings { get; set; }
+    }
+}
