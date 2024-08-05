b8c312914ab07dc9ca23a87b722204d8d475df52
calKU0 <228528@student.uek.krakow.pl>
2024-08-05 21:58:14 +0200

    add Statistics page

---
+++ MemoriesWebApp/Controllers/StatisticsController.cs
@@ -0,0 +1,22 @@
+﻿using MemoriesWebApp.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace MemoriesWebApp.Controllers
+{
+    public class StatisticsController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public StatisticsController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var statistics = _context.GetStatistics();
+            return View(statistics);
+        }
+    }
+}
---
+++ MemoriesWebApp/ViewModels/StatisticsViewModel.cs
@@ -0,0 +1,28 @@
+﻿using MemoriesWebApp.Models;
+
+namespace MemoriesWebApp.ViewModels
+{
+    public class StatisticsViewModel
+    {
+        public decimal AvgMeetingTime { get; set; }
+        public decimal AvgPicturesTaken { get; set; }
+        public int MeetingMostPictures { get; set; }
+
+        public List<Meeting> Meetings { get; set; }
+        public List<MeetingTimeSpent> TimeSpent { get; set; }
+        public List<Image> Images { get; set; }
+        public List<PicturesTakenPerMeeting> PicturesTakenPerMeeting { get; set; }
+    }
+    public class MeetingTimeSpent
+    {
+        public int MeetingId { get; set; }
+        public int Days { get; set; }
+        public int Hours { get; set; }
+    }
+
+    public class PicturesTakenPerMeeting
+    {
+        public int MeetingId { get; set; }
+        public int PicturesTaken { get; set; }
+    }
+}
