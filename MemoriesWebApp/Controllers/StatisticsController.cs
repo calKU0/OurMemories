0d6443485745f8edab777003a1c5cfdf7bfcb8ea
calKU0 <228528@student.uek.krakow.pl>
2024-08-07 22:46:52 +0200

    fix mobile layout

--- MemoriesWebApp/Controllers/StatisticsController.cs
+++ MemoriesWebApp/Controllers/StatisticsController.cs
@@ -15,6 +15,16 @@ namespace MemoriesWebApp.Controllers
 
         public IActionResult Index()
         {
+            var brows = Request.Headers["User-Agent"].ToString();
+            bool isMobileDevice = brows != null && (
+                brows.IndexOf("Mobile", StringComparison.OrdinalIgnoreCase) >= 0 ||
+                brows.Contains("Android") ||
+                brows.Contains("iPhone") ||
+                brows.Contains("Windows Phone")
+            );
+
+            ViewBag.IsMobileDevice = isMobileDevice;
+
             var statistics = _context.GetStatistics();
             return View(statistics);
         }
