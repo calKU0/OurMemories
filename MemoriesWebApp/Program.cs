3dc8b7ab7206a595e22d507d91692cf16bf27c19
calKU0 <228528@student.uek.krakow.pl>
2024-09-03 19:42:35 +0200

    change app builder

--- MemoriesWebApp/Program.cs
+++ MemoriesWebApp/Program.cs
@@ -36,7 +36,7 @@ if (args.Length == 1 && args[0].ToLower() == "seeddata")
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error");
+    //app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
