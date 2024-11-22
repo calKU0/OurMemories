ef97e5f9c418da6e4f64269205c4247876ded0a9
calKU0 <228528@student.uek.krakow.pl>
2024-11-22 10:36:48 +0100

    add description to meeting

--- MemoriesWebApp/ViewModels/CreateMeetingViewModel.cs
+++ MemoriesWebApp/ViewModels/CreateMeetingViewModel.cs
@@ -8,6 +8,7 @@ namespace MemoriesWebApp.ViewModels
         public DateTime DateStart { get; set; }
         public DateTime DateEnd { get; set; }
         public MeetingCity MeetingCity { get; set; }
+        public string? Description { get; set; }
         public IFormFile? Image { get; set; }
         public bool Realized { get; set; }
     }
